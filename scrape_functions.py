189f1cd2d4d053ff44641472c7c67bb1f1c28ccf
Alec Morgan <alecnmorgan@gmail.com>
2019-04-26 16:27:47 -0700

    Add function for navigating to set of Fortune 500 companies

--- scrape_functions.py
+++ scrape_functions.py
@@ -1,27 +1,44 @@
 import requests
+from selenium.webdriver import Firefox, Chrome
+from selenium.webdriver.common.keys import Keys
 
-def get_comic_title(browser):
-    """Return the title text"""
-    sel = "div#ctitle"
-    title = browser.find_element_by_css_selector(sel)
-    return title.text
 
-def get_prev(browser):
-    """Go to the previous comic"""
-    #nav_buttons = browser.find_elements_by_css_selector(sel)
-    #prev_button = nav_buttons[1]
-    prev_button = browser.find_element_by_link_text("< Prev")
-    prev_button.click()
+def scrape_company_data(companies):
+    """Scrape data from Glassdoor on companies in list."""
+    browser = Chrome()
+    url = "https://www.glassdoor.com/Reviews/index.htm"
+    browser.get(url)
 
-def scrape_image(img, filename):
-    """Save an image element as filename"""
-    response = requests.get(img.get_attribute('src'))
-    img_data = response.content
-    with open(filename, 'wb') as f:
-        f.write(img_data)
+    for company in companies:
+        sel = "input#KeywordSearch.keyword"
+        search_bar = browser.find_element_by_css_selector(sel)
 
-def find_image(browser):
-    """Return the image element of the xkcd page."""
-    sel = "div#comic img"
-    img = browser.find_element_by_css_selector(sel)
-    return img
+        search_bar.send_keys(company)
+        search_bar.send_keys(Keys.ENTER)
+
+        if len(browser.window_handles) > 1:
+            tab_1, tab_2 = browser.window_handles
+            browser.switch_to.window(tab_1)
+            browser.close()
+            browser.switch_to.window(tab_2)
+
+        sel = "a.tightAll.h2"
+        company_link = browser.find_element_by_css_selector(sel)
+        company_link.click()
+
+        sel = "a.eiCell.cell.reviews"
+        reviews_link = browser.find_element_by_css_selector(sel)
+        reviews_link.click()
+
+        dummy_function()
+        browser.get(url)
+        
+        
+def dummy_function():
+    """Stand-in for Hussein and Lee's work."""
+    pass
+
+
+def get_fortune_500():
+    """Stand-in for Kaspar's work."""
+    return ["Google", "Amazon", "AMD"]
